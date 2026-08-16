f3fb2e72c34f89d8dd43ac3b6edb285c09c1b612
reymodetecno-svg <reymodetecno@gmail.com>
2026-08-17 05:18:33 +0700

    Update asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "yamada-downloader-v1";
-const ASSETS = ["/","/index.html","/style.css","/app.js","/manifest.webmanifest","/assets/LOGO.jpg","/assets/banner-placeholder.svg"];
+const ASSETS = ["/","/index.html","/style.css","/app.js","/manifest.webmanifest","/LOGO.jpg","/banner-placeholder.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", e => {
