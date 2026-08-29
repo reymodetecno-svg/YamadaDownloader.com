61ee3399c79b9809414a24a5eddb5d5967fa3829
reymodetecno-svg <reymodetecno@gmail.com>
2026-08-30 04:33:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,58 +1,6 @@
-const CACHE = "yamada-downloader-v3";
-
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/LOGO.jpg",
-  "/banner.mp4",
-  "/logo.svg",
-  "/manifest.webmanifest"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .catch(err => {
-        console.error("Cache error:", err);
-      })
-  );
-
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (response.ok) {
-          const copy = response.clone();
-
-          caches.open(CACHE).then(cache => {
-            cache.put(event.request, copy);
-          });
-        }
-
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11684194
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
